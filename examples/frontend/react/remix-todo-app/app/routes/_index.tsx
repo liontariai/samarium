df87743dc5832dd99c5f64e5ca1f1004be039f92
liontari.ai <sj@liontari.ai>
2024-09-29 14:23:25 +0200

    refactor

--- examples/frontend/react/remix-todo-app/app/routes/_index.tsx
+++ examples/frontend/react/remix-todo-app/app/routes/_index.tsx
@@ -62,6 +62,22 @@ const TodoInput = ({
     </div>
 );
 
+const TrashIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+        />
+    </svg>
+);
 const TodoItem = ({
     todo,
     mutateTodos,
@@ -122,20 +138,7 @@ const TodoItem = ({
             }
             className="ml-auto text-gray-400 hover:text-red-500 transition duration-300 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 rounded-full p-1"
         >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-            </svg>
+            <TrashIcon />
         </button>
     </motion.li>
 );
