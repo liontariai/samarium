8ef1935f234cf1ca5f83d58836f519721eb37e82
liontari.ai <sj@liontari.ai>
2024-09-29 15:44:49 +0200

    add meta tag

--- examples/frontend/react/remix-todo-app/app/root.tsx
+++ examples/frontend/react/remix-todo-app/app/root.tsx
@@ -20,6 +20,7 @@ export default function App() {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
+                <meta name="description" content="Todo App" />
                 <title>Todo App</title>
                 <Meta />
                 <Links />
