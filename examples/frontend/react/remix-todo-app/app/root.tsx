8e27251d1d44695b3eb11057058883e3e771b857
Liontari <lionatri-ai@pm.me>
2024-09-29 14:41:10 +0200

    polish

--- examples/frontend/react/remix-todo-app/app/root.tsx
+++ examples/frontend/react/remix-todo-app/app/root.tsx
@@ -20,6 +20,7 @@ export default function App() {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
+                <title>Todo App</title>
                 <Meta />
                 <Links />
             </head>
