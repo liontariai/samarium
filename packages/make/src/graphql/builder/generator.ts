0156cc39b8bb8d2214103c347db72b5e98264d61
Liontari <lionatri-ai@pm.me>
2024-12-21 17:55:31 +0100

    print types for ObjectTypes, if custom scalars were collected, as these may reference some of these ObjectTypes in their TS typedef

--- packages/make/src/graphql/builder/generator.ts
+++ packages/make/src/graphql/builder/generator.ts
@@ -99,7 +99,11 @@ export class Generator {
                 .map(([_, code]) => code)
                 .filter((code, index, arr) => arr.indexOf(code) === index),
             ...[...collector.selectionTypes.entries()]
-                .filter(([type]) => type.isInput)
+                .filter(
+                    ([type]) =>
+                        type.isInput ||
+                        (collector.customScalars.size > 0 && type.isObject),
+                )
                 .map(([_, code]) => code)
                 .filter((code, index, arr) => arr.indexOf(code) === index),
             this.Codegen.EnumTypesMapped(collector),
